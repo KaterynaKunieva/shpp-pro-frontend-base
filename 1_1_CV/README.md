ff67e44b018b6248286a333040c8cd84788df89c
Kateryna <kunieva.kateryna@gmail.com>
2026-05-09 18:46:06 +0200

    1.1 CV

---
+++ 1_1_CV/README.md
@@ -0,0 +1,13 @@
+# CV
+
+Responsive one-page CV website with a minimalistic design.
+
+## Tech Stack
+
+- HTML5
+- CSS3
+- JavaScript
+
+## Preview
+
+Live demo: [Github Pages](https://katerynakunieva.github.io/shpp-pro-frontend-base/1_1_CV/)
---
+++ 1_1_CV/assets/js/main.js
@@ -0,0 +1,46 @@
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll(".calculate-period").forEach(item => {
+    const rawText = item.textContent.trim();
+    const [from, till] = rawText.split("-").map(item => item.trim());
+    const fromDate = parseDate(from);
+    const tillDate = parseDate(till);
+    const diffText = formatDiff(fromDate, tillDate);
+    const spanEl = document.createElement("span");
+    spanEl.textContent = diffText;
+    item.appendChild(spanEl);
+  });
+});
+
+const parseDate = (str) => {
+  if (str == "now") {
+    return new Date();
+  }
+  if (!str || !str.includes("/")) {
+    return null;
+  }
+  const [month, year] = str.split("/").map(Number);
+  return new Date(year, month - 1, 1);
+}
+
+function formatDiff(start, end) {
+  if (!start || !end) {
+    return `1міс.`
+  }
+  let years = end.getFullYear() - start.getFullYear();
+  let months = end.getMonth() - start.getMonth();
+
+  if (months < 0) {
+    years--;
+    months += 12;
+  }
+
+  if (years === 0) {
+    return `${months}міс.`;
+  }
+
+  if (months === 0) {
+    return `${years}р.`;
+  }
+
+  return `${years}р. ${months}міс.`;
+}
